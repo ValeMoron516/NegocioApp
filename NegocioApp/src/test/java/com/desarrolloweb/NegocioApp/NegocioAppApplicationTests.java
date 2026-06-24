9a8b82c0b09e1e9401db8d901e076344fd834262
ValeMoron516 <valemoron516@gmail.com>
2026-06-23 21:10:05 -0300

    Add files via upload

---
+++ NegocioApp/src/test/java/com/desarrolloweb/NegocioApp/NegocioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrolloweb.NegocioApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegocioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
