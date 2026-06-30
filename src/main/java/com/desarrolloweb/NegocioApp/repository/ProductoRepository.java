004f69e1e5c909333e815bac53d04702f216e3c5
Enzo Lombardo <e156089@gmail.com>
2026-06-30 13:50:41 -0300

    Se actualiza el package de los archivos .java

--- src/main/java/com/desarrolloweb/NegocioApp/repository/ProductoRepository.java
+++ src/main/java/com/desarrolloweb/NegocioApp/repository/ProductoRepository.java
@@ -1,9 +1,9 @@
-package com.backend.javaCrudApp.repository;
+package com.desarrolloweb.NegocioApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.backend.javaCrudApp.entity.Producto;
+import com.desarrolloweb.NegocioApp.entity.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
