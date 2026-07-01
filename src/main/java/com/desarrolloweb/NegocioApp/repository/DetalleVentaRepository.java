b0f0cdabd7bf7cf82c8124d70cd6c746bcea62b5
Benjamin <filippinibenja678@gmail.com>
2026-06-30 21:01:05 -0300

    se modificaron los nombres de los endpoints y se cambio el contenido de los repository

--- src/main/java/com/desarrolloweb/NegocioApp/repository/DetalleVentaRepository.java
+++ src/main/java/com/desarrolloweb/NegocioApp/repository/DetalleVentaRepository.java
@@ -1,9 +1,17 @@
 package com.desarrolloweb.NegocioApp.repository;
 
-import com.desarrolloweb.NegocioApp.entity.DetalleVenta;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.desarrolloweb.NegocioApp.entity.DetalleVenta;
+
 @Repository
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+
+    List<DetalleVenta> findByVentaId(Long ventaId);
+
+    List<DetalleVenta> findByProductoId(Long productoId);
+
 }
--- src/main/java/com/desarrolloweb/NegocioApp/repository/ValoracionNegocioRepository.java
+++ src/main/java/com/desarrolloweb/NegocioApp/repository/ValoracionNegocioRepository.java
@@ -1,9 +1,19 @@
 package com.desarrolloweb.NegocioApp.repository;
 
-import com.desarrolloweb.NegocioApp.entity.ValoracionNegocio;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.desarrolloweb.NegocioApp.entity.ValoracionNegocio;
+
 @Repository
 public interface ValoracionNegocioRepository extends JpaRepository<ValoracionNegocio, Long> {
+
+    List<ValoracionNegocio> findByNegocioId(Long negocioId);
+
+    List<ValoracionNegocio> findByClienteId(Long clienteId);
+
+    List<ValoracionNegocio> findByEstrellas(Integer estrellas);
+
 }
