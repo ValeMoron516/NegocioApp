d1aa2187f06844e85d321a76505faf302c9aaf7b
Enzo Lombardo <e156089@gmail.com>
2026-08-18 21:01:31 -0300

    Se agrega la estructura logica de los endpoints de Productos

--- src/main/java/com/desarrolloweb/NegocioApp/controller/ProductoController.java
+++ src/main/java/com/desarrolloweb/NegocioApp/controller/ProductoController.java
@@ -11,16 +11,75 @@ import com.desarrolloweb.NegocioApp.entity.Producto;
 import com.desarrolloweb.NegocioApp.service.ProductoService;
 
 @RestController
-@RequestMapping("/producto")
+@RequestMapping("api/v1/productos")
 public class ProductoController {
 
     @Autowired
     private ProductoService productoService;
 
+// ##################################################
+
+    // Leer todos los productos (paginadas)
     @GetMapping
-    public List<Producto> obtenerTodosProductos() {
-        return productoService.obtenerTodosProductos();
+    public ResponseEntity<PaginacionDTO<ProductoDTO>> obtenerTodosProductos(
+        @RequestParam(defaultValue = "1") Integer page, 
+        @RequestParam(defaultValue = "20") Integer limit) {
+        return ResponseEntity.status(HttpStatus.OK).body(categoriaService.obtenerTodosProductos(page, limit)); // 200
     }
 
+// ##################################################
+
+    // Leer producto por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductoDTO> obtenerCategoriaPorId(@PathVariable Long id) {
+        try {
+            ProductoDTO pDto = categoriaService.obtenerPtoductoPorId(id);
+            // Existe
+            return ResponseEntity.status(HttpStatus.OK).body(pDto); // 200
+        } catch (NotFoundException e) {
+            // No existe
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // 404
+        }
+    }
+
+// ##################################################
+
+    // Crear nuevo producto
+    @PostMapping
+    public ResponseEntity<ProductoDTO> crearPtoducto(
+        @RequestBody Producto p) {
+        
+        try {
+            ProductoDTO pDto = categoriaService.crearProducto(p);
+            return ResponseEntity.status(HttpStatus.CREATED).body(pDto); // 201
+        }
+        catch (BadRequestException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // 400
+        }
+        catch (ConflictException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build(); // 409
+        }
+    }
+
+// ##################################################
+
+    // Actualizar por id
+    @PutMapping("{id}")
+    public ResponseEntity<ProductoDTO> actualizarProductoPorId(@PathVariable Long id, @RequestBody Producto producto) {
+        categoriaService.actualizarProductoPorId(id, categoria);
+    }
     
+// ##################################################
+
+    // Borrar por ID
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> borrarProductoPorId(@PathVariable Long id) {
+        try {
+            productoService.borrarProductoPorId(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // 204
+        }
+        catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // 404
+        }
+    }
 }
--- src/main/java/com/desarrolloweb/NegocioApp/service/ProductoService.java
+++ src/main/java/com/desarrolloweb/NegocioApp/service/ProductoService.java
@@ -14,8 +14,132 @@ public class ProductoService {
     @Autowired
     private ProductoRepository productoRepository;
 
-    public List<Producto> obtenerTodosProductos() {
-        return productoRepository.findAll();
-    }
+// ##################################################
+
+    // Obtener lista de productos (paginadas)
+    public PaginacionDTO<ProductoDTO> obtenerTodosProductos(Integer page, Integer limit) { 
+        
+        Pageable pageable = PageRequest.of(page - 1, limit);
+        Page<Producto> paginaProductos = productoRepository.findAll(pageable);
+        List<Producto> productos = paginaProductoss.getContent();
+        
+        List<ProductoDTO> dtos = new ArrayList<>();
+        for (Producto p : productos) {
+            ProductoDTO dto = new ProductoDTO();
+            dto.setId(p.getId());
+            dto.setNegocioId(p.getNegocioId());
+            dto.setNombreNegocio(p.getNombreNegocio());
+            dto.setCategoriaId(p.getCategoriaId());
+            dto.setNombreCategoria(p.getNombreCategoria());
+            dto.setNombre(p.getNombre());
+            dto.setDescripcion(p.getDescripcion());
+            dto.setPrecio(p.getPrecio());
+            dto.setStock(p.getStock());
+            
+            dtos.add(dto);
+        }
+        
+        MetaDTO meta = new MetaDTO(
+            paginaProductos.getTotalElements(),     // totalItems   (elementos totales)
+            paginaProductos.getNumberOfElements(),  // itemCount    (elementos en la página actual)
+            paginaProductos.getSize(),              // itemsPerPage (elementos maximos por pagina)
+            paginaProductos.getTotalPages(),        // totalPages   (total de paginas)
+            paginaProductos.getNumber() + 1         // currentPage  (pagina actual)
+        );
     
+        return new PaginacionDTO<>(dtos, meta);
+    }
+
+// ##################################################
+
+    // Obtener producto por ID
+    public ProductoDTO obtenerProductoPorId(Long id) { 
+        Optional<Producto> optP = productoRepository.findById(id);
+        
+        // Existe
+        if (optP.isPresent()) {
+            Producto p = optP.get();
+            ProductoDTO dto = new ProductoDTO();
+            
+            dto.setId(p.getId());
+            dto.setNegocioId(p.getNegocioId());
+            dto.setNombreNegocio(p.getNombreNegocio());
+            dto.setCategoriaId(p.getCategoriaId());
+            dto.setNombreCategoria(p.getNombreCategoria());
+            dto.setNombre(p.getNombre());
+            dto.setDescripcion(p.getDescripcion());
+            dto.setPrecio(p.getPrecio());
+            dto.setStock(p.getStock());
+            
+            return dto;
+        }
+        
+        // No existe
+        throw new NotFoundException("El elemento solicitado no existe");
+    }
+
+// ##################################################
+
+    // Crear nuevo producto
+    public ProductoDTO crearProducto(Producto p) {
+
+        // nombre nulo o vacio
+        if (p.nombre == null || p.nombre.isEmpty()) { throw new BadRequestException("Nombre invalido"); }
+        
+        // descripcion nulo o vacio
+        if (p.descripcion == null || p.descripcion.isEmpty()) { throw new BadRequestException("Descripcion invalida"); }
+        
+        // TERMINAR
+        dto.setId(p.getId());
+            dto.setNegocioId(p.getNegocioId());
+            dto.setNombreNegocio(p.getNombreNegocio());
+            dto.setCategoriaId(p.getCategoriaId());
+            dto.setNombreCategoria(p.getNombreCategoria());
+            
+            dto.setPrecio(p.getPrecio());
+            dto.setStock(p.getStock());
+        // TERMINAR
+        
+        Producto pResp = productoRepository.save(p);
+        
+        return new ProductoDTO(pResp.getId(), pResp.getNombre());
+        }
+    }
+
+// ##################################################
+
+    // Actualizar producto
+    public ProductoDTO actualizarProductoPorId(Long id, Producto p) {
+        Optional<Producto> optP = productoRepository.findById(id);
+
+        if (optP.isPresent()) {
+            Producto newP = optP.get();
+            if (p.getNombre() != null || !p.getNombre().isBlank()) {
+                newP.setNombre(p.getNombre());
+            }
+            if (p.getDescripcion() != null || !p.getDescripcion().isBlank()) {
+                newP.setDescripcion(p.getDescripcion());
+            }
+            // TERMINAR PARA CADA ATRIBUTO
+            
+            productoRepository.save(newP);
+            Producto p = productoRepository.findById(newP.getId()).get();
+            return new ProductoDTO(p.getId(), p.getNombre(), p.getDescripcion());
+        }
+        else {
+            throw new NotFoundException("La categoria con el ID provisto no existe");  
+   }
+
+// ##################################################
+
+    // Borrar producto por id
+    public void borrarProductoPorId(Long id) {
+         
+         Optional<Producto> optP = productoRepository.findById(id);
+
+        if (optP.isPresent()) {
+            productoRepository.deleteById(id);
+        } else {
+            throw new NotFoundException("La categoria con el ID provisto no existe");
+    }
 }
