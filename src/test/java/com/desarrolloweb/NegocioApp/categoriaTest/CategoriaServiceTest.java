d9bda192d427cd1b3dc399284dc457e0bece557e
Enzo Lombardo <e156089@gmail.com>
2026-09-01 19:44:35 -0300

    Se agregan los tests para el Service de crear Categorias

--- src/test/java/com/desarrolloweb/NegocioApp/categoriaTest/CategoriaServiceTest.java
+++ src/test/java/com/desarrolloweb/NegocioApp/categoriaTest/CategoriaServiceTest.java
@@ -1,6 +1,7 @@
 package com.desarrolloweb.NegocioApp.categoriaTest;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -13,6 +14,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import com.desarrolloweb.NegocioApp.entity.Categoria;
+import com.desarrolloweb.NegocioApp.dtos.categoriaDTO.CategoriaResponseDTO;
 import com.desarrolloweb.NegocioApp.exception.ConflictException;
 import com.desarrolloweb.NegocioApp.exception.NotFoundException;
 import com.desarrolloweb.NegocioApp.repository.CategoriaRepository;
@@ -22,59 +27,90 @@ import com.desarrolloweb.NegocioApp.service.CategoriaService;
 @ExtendWith(MockitoExtension.class)
 public class CategoriaServiceTest {
 
-    @Mock
-    CategoriaRepository categoriaRepository; // Modulo simulado
+	@Mock
+	CategoriaRepository categoriaRepository; // Modulo simulado
 
-    @Mock
-    ProductoRepository productoRepository; // Modulo simulado
+	@Mock
+	ProductoRepository productoRepository; // Modulo simulado
 
-    @InjectMocks
-    CategoriaService categoriaService; // Modulo principal
-    
-    // ##################################################
-    // ##################################################
-    // ##################################################
-    // ##################################################
+	@InjectMocks
+	CategoriaService categoriaService; // Modulo principal
 
-    @Test
-    void borrarCategoriaPorId_Inexistente() {
-        Long id = 1L;
-        when(categoriaRepository.existsById(id)).thenReturn(false);
-
-        assertThrows(NotFoundException.class, () -> {
-            categoriaService.borrarCategoriaPorId(id);
+	// ##################################################
+	// ##################################################
+	
+	@Test
+	void obtenerCategoriaPorId_Exitoso() {
+	    Long id = 1L;
+	    Categoria respuesta = new Categoria(1L, "Hogar", "Productos para el hogar");
+	    when(categoriaRepository.findById(id)).thenReturn(Optional.of(respuesta));
+	    
+	    CategoriaResponseDTO respuestaService = categoriaService.obtenerCategoriaPorId(id);
+	    
+	    assertEquals(respuesta.getId(), respuestaService.getId());
+	    assertEquals(respuesta.getNombre(), respuestaService.getNombre());
+	    assertEquals(respuesta.getDescripcion(), respuestaService.getDescripcion());
+	    
+	    verify(categoriaRepository, times(1)).findById(id);
+	}
+	
+	@Test
+	void obtenerCategoriaPorId_Invalido() {
+	    Long id = 1L;
+	    when(categoriaRepository.findById(id)).thenReturn(Optional.empty());
+	    
+	    Exception excepcion = assertThrows(NotFoundException.class, () -> {
+            categoriaService.obtenerCategoriaPorId(id); 
         });
+        
+        assertEquals("El elemento solicitado no existe", excepcion.getMessage());
+        verify(categoriaRepository, times(1)).findById(id);
+	}
+	
+	// ##################################################
+	// ##################################################
 
-        verify(categoriaRepository, times(1)).existsById(anyLong());
-        verify(productoRepository, never()).existsByCategoriaId(anyLong());
-        verify(categoriaRepository, never()).deleteById(anyLong());
-    }
+	@Test
+	void borrarCategoriaPorId_Inexistente() {
+		Long id = 1L;
+		when(categoriaRepository.existsById(id)).thenReturn(false);
 
-    @Test
-    void borrarCategoriaPorId_ProductosAsociados() {
-        Long id = 1L;
-        when(categoriaRepository.existsById(id)).thenReturn(true);
-        when(productoRepository.existsByCategoriaId(id)).thenReturn(true);
+		Exception excepcion = assertThrows(NotFoundException.class, () -> {
+			categoriaService.borrarCategoriaPorId(id);
+		});
 
-        assertThrows(ConflictException.class, () -> {
-            categoriaService.borrarCategoriaPorId(id);
-        });
+        assertEquals("La categoria con el ID provisto no existe", excepcion.getMessage());
+		verify(categoriaRepository, times(1)).existsById(anyLong());
+		verify(productoRepository, never()).existsByCategoriaId(anyLong());
+		verify(categoriaRepository, never()).deleteById(anyLong());
+	}
 
-        verify(categoriaRepository, times(1)).existsById(anyLong());
-        verify(productoRepository, times(1)).existsByCategoriaId(anyLong());
-        verify(categoriaRepository, never()).deleteById(anyLong());
-    }
+	@Test
+	void borrarCategoriaPorId_ProductosAsociados() {
+		Long id = 1L;
+		when(categoriaRepository.existsById(id)).thenReturn(true);
+		when(productoRepository.existsByCategoriaId(id)).thenReturn(true);
 
-    @Test
-    void borrarCategoriaPorId_Exitoso() {
-        Long id = 1L;
-        when(categoriaRepository.existsById(id)).thenReturn(true);
-        when(productoRepository.existsByCategoriaId(id)).thenReturn(false);
+		Exception excepcion = assertThrows(ConflictException.class, () -> {
+			categoriaService.borrarCategoriaPorId(id);
+		});
+		
+        assertEquals("La categoria provista no puede eliminarse debido a que tiene productos asociados", excepcion.getMessage());
+		verify(categoriaRepository, times(1)).existsById(anyLong());
+		verify(productoRepository, times(1)).existsByCategoriaId(anyLong());
+		verify(categoriaRepository, never()).deleteById(anyLong());
+	}
 
-        categoriaService.borrarCategoriaPorId(id);
+	@Test
+	void borrarCategoriaPorId_Exitoso() {
+		Long id = 1L;
+		when(categoriaRepository.existsById(id)).thenReturn(true);
+		when(productoRepository.existsByCategoriaId(id)).thenReturn(false);
 
-        verify(categoriaRepository, times(1)).existsById(anyLong());
-        verify(productoRepository, times(1)).existsByCategoriaId(anyLong());
-        verify(categoriaRepository, times(1)).deleteById(anyLong());
-    }
+		categoriaService.borrarCategoriaPorId(id);
+
+		verify(categoriaRepository, times(1)).existsById(anyLong());
+		verify(productoRepository, times(1)).existsByCategoriaId(anyLong());
+		verify(categoriaRepository, times(1)).deleteById(anyLong());
+	}
 }
