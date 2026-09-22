9af109c88a4b7cefe40295113f13d1fa397954f8
Benjamin <filippinibenja678@gmail.com>
2026-09-22 19:03:16 -0300

    se agrego los DTO de "valoracion negocio"

--- src/main/java/com/desarrolloweb/NegocioApp/dtos/ValoracionNegocioDTO/ValoracionNegocioRequest.java
+++ src/main/java/com/desarrolloweb/NegocioApp/dtos/ValoracionNegocioDTO/ValoracionNegocioRequest.java
@@ -1,5 +1,52 @@
 package com.desarrolloweb.NegocioApp.dtos.ValoracionNegocioDTO;
 
 public class ValoracionNegocioRequest {
-    
+
+	private Long negocioId;
+	private Long clienteId;
+	private Integer estrellas;
+	private String comentario;
+
+	public ValoracionNegocioRequest() {
+	}
+
+	public ValoracionNegocioRequest(Long negocioId, Long clienteId,
+			Integer estrellas, String comentario) {
+		this.negocioId = negocioId;
+		this.clienteId = clienteId;
+		this.estrellas = estrellas;
+		this.comentario = comentario;
+	}
+
+	public Long getNegocioId() {
+		return negocioId;
+	}
+
+	public void setNegocioId(Long negocioId) {
+		this.negocioId = negocioId;
+	}
+
+	public Long getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(Long clienteId) {
+		this.clienteId = clienteId;
+	}
+
+	public Integer getEstrellas() {
+		return estrellas;
+	}
+
+	public void setEstrellas(Integer estrellas) {
+		this.estrellas = estrellas;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
 }
--- src/main/java/com/desarrolloweb/NegocioApp/dtos/ValoracionNegocioDTO/ValoracionNegocioResponse.java
+++ src/main/java/com/desarrolloweb/NegocioApp/dtos/ValoracionNegocioDTO/ValoracionNegocioResponse.java
@@ -1,5 +1,74 @@
 package com.desarrolloweb.NegocioApp.dtos.ValoracionNegocioDTO;
 
+import java.time.LocalDateTime;
+
 public class ValoracionNegocioResponse {
-    
+
+	private Long id;
+	private Long negocioId;
+	private Long clienteId;
+	private Integer estrellas;
+	private String comentario;
+	private LocalDateTime fecha;
+
+	public ValoracionNegocioResponse() {
+	}
+
+	public ValoracionNegocioResponse(Long id, Long negocioId, Long clienteId,
+			Integer estrellas, String comentario, LocalDateTime fecha) {
+		this.id = id;
+		this.negocioId = negocioId;
+		this.clienteId = clienteId;
+		this.estrellas = estrellas;
+		this.comentario = comentario;
+		this.fecha = fecha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNegocioId() {
+		return negocioId;
+	}
+
+	public void setNegocioId(Long negocioId) {
+		this.negocioId = negocioId;
+	}
+
+	public Long getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(Long clienteId) {
+		this.clienteId = clienteId;
+	}
+
+	public Integer getEstrellas() {
+		return estrellas;
+	}
+
+	public void setEstrellas(Integer estrellas) {
+		this.estrellas = estrellas;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
 }
